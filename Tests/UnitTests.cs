eeec4d2922f076822f52216da11d55702dcca776
Jesse Malmkar <jmalmkar2@huskers.unl.edu>
2026-03-25 20:02:20 -0500

    add name to test

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -4,7 +4,7 @@ namespace GithubActionsLab;
 public class Addition
 {
 	[TestMethod]
-	public void Add_Valid_Patino()
+	public void Add_Valid_Patino_JesseMalmkar()
 	{
 		Assert.AreEqual(3, Program.Add("1", "2"));
 		Assert.AreEqual(5, Program.Add("3", "2"));
@@ -12,7 +12,7 @@ public class Addition
 	}
 
 	[TestMethod]
-	public void Add_Invalid_Patino()
+	public void Add_Invalid_Patino_JesseMalmkar()
 	{
 		Assert.ThrowsException<FormatException>(() => Program.Add("1", "a"));
 		Assert.ThrowsException<FormatException>(() => Program.Add("a", "1"));
@@ -20,7 +20,7 @@ public class Addition
 	}
 
 	[TestMethod]
-	public void Add_Null_Patino()
+	public void Add_Null_Patino_JesseMalmkar()
 	{
 		Assert.ThrowsException<ArgumentNullException>(() => Program.Add("1", null));
 		Assert.ThrowsException<ArgumentNullException>(() => Program.Add(null, "1"));
@@ -32,7 +32,7 @@ public class Addition
 public class Subtract
 {
     [TestMethod]
-    public void Sub_Valid_Patino()
+    public void Sub_Valid_Patino_JesseMalmkar()
     {
         Assert.AreEqual(-1, Program.Subtract("1", "2"));
         Assert.AreEqual(1, Program.Subtract("3", "2"));
@@ -40,7 +40,7 @@ public class Subtract
     }
 
     [TestMethod]
-    public void Sub_Invalid_Patino()
+    public void Sub_Invalid_Patino_JesseMalmkar()
     {
         Assert.ThrowsException<FormatException>(() => Program.Subtract("1", "a"));
         Assert.ThrowsException<FormatException>(() => Program.Subtract("a", "1"));
@@ -48,7 +48,7 @@ public class Subtract
     }
 
     [TestMethod]
-    public void Sub_Null_Patino()
+    public void Sub_Null_Patino_JesseMalmkar()
     {
         Assert.ThrowsException<ArgumentNullException>(() => Program.Subtract("1", null));
         Assert.ThrowsException<ArgumentNullException>(() => Program.Subtract(null, "1"));
@@ -60,7 +60,7 @@ public class Subtract
 public class Multiply
 {
     [TestMethod]
-    public void Mult_Valid_Patino()
+    public void Mult_Valid_Patino_JesseMalmkar()
     {
         Assert.AreEqual(2, Program.Multiply("1", "2"));
         Assert.AreEqual(6, Program.Multiply("3", "2"));
@@ -68,7 +68,7 @@ public class Multiply
     }
 
     [TestMethod]
-    public void Mult_Invalid_Patino()
+    public void Mult_Invalid_Patino_JesseMalmkar()
     {
         Assert.ThrowsException<FormatException>(() => Program.Multiply("1", "a"));
         Assert.ThrowsException<FormatException>(() => Program.Multiply("a", "1"));
@@ -76,7 +76,7 @@ public class Multiply
     }
 
     [TestMethod]
-    public void Mult_Null_Patino()
+    public void Mult_Null_Patino_JesseMalmkar()
     {
         Assert.ThrowsException<ArgumentNullException>(() => Program.Multiply("1", null));
         Assert.ThrowsException<ArgumentNullException>(() => Program.Multiply(null, "1"));
@@ -88,7 +88,7 @@ public class Multiply
 public class Divide
 {
     [TestMethod]
-    public void Div_Valid_Patino()
+    public void Div_Valid_Patino_JesseMalmkar()
     {
         Assert.AreEqual(0.5, Program.Divide("1", "2"));
         Assert.AreEqual(1.5, Program.Divide("3", "2"));
@@ -96,7 +96,7 @@ public class Divide
     }
 
     [TestMethod]
-    public void Div_Invalid_Patino()
+    public void Div_Invalid_Patino_JesseMalmkar()
     {
         Assert.ThrowsException<FormatException>(() => Program.Divide("1", "a"));
         Assert.ThrowsException<FormatException>(() => Program.Divide("a", "1"));
@@ -104,7 +104,7 @@ public class Divide
     }
 
     [TestMethod]
-    public void Div_Null_Patino()
+    public void Div_Null_Patino_JesseMalmkar()
     {
         Assert.ThrowsException<ArgumentNullException>(() => Program.Divide("1", null));
         Assert.ThrowsException<ArgumentNullException>(() => Program.Divide(null, "1"));
@@ -116,7 +116,7 @@ public class Divide
 public class Power
 {
     [TestMethod]
-    public void Pow_Valid_Patino()
+    public void Pow_Valid_Patino_JesseMalmkar()
     {
         Assert.AreEqual(1, Program.Power("1", "2"));
         Assert.AreEqual(9, Program.Power("3", "2"));
@@ -124,7 +124,7 @@ public class Power
     }
 
     [TestMethod]
-    public void Pow_Invalid_Patino()
+    public void Pow_Invalid_Patino_JesseMalmkar()
     {
         Assert.ThrowsException<FormatException>(() => Program.Power("1", "a"));
         Assert.ThrowsException<FormatException>(() => Program.Power("a", "1"));
@@ -132,7 +132,7 @@ public class Power
     }
 
     [TestMethod]
-    public void Pow_Null_Patino()
+    public void Pow_Null_Patino_JesseMalmkar()
     {
         Assert.ThrowsException<ArgumentNullException>(() => Program.Power("1", null));
         Assert.ThrowsException<ArgumentNullException>(() => Program.Power(null, "1"));
